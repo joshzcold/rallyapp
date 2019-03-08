9281a5e3cf6d3fd90f586bdf9b6c11047aa20aa0
Joshua Cold <joshua.cold@microfocus.com>
2019-03-08 15:48:41 -0700

    Added navigation bar file for use

---
+++ lib/screens/navigationBar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatefulWidget{
+  NavigationBar({Key key}) : super(key: key);
+
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar>{
+  int _selectedIndex = 1;
+  final _widgetOptions = [
+    Text('Index 0: Home'),
+    Text('Index 1: Business'),
+    Text('Index 2: School'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+            BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('Business')),
+            BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('School')),
+          ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
