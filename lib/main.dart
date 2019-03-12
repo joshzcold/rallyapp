66b0d3251c01e56be479ccedd1ad654b9c4b6a35
Joshua Cold <joshua.cold@microfocus.com>
2019-03-12 11:20:06 -0600

    Renamed some screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rallyapp/screens/login.dart';
-import 'package:rallyapp/screens/settings.dart';
+import 'package:rallyapp/screens/loginScreen.dart';
+import 'package:rallyapp/screens/settingsScreen.dart';
 import 'package:rallyapp/screens/mainScreen.dart';
 import 'package:rallyapp/blocs/friends/friends.dart';
 import 'package:rallyapp/blocs/app/navigationBloc.dart';
