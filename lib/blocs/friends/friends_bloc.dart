6b34e36ea50b46ad51f98deee793d1dd1b2f4094
Joshua Cold <joshzcold@gmail.com>
2019-03-12 06:32:20 -0600

    Added back Friends Loaded/ Loading to the Bloc logic.. Added friendList items to the Friends Screen

--- lib/blocs/friends/friends_bloc.dart
+++ lib/blocs/friends/friends_bloc.dart
@@ -4,25 +4,26 @@ import 'package:rallyapp/blocs/friends/friends.dart';
 
 Map<dynamic, dynamic> friends = {};
 
-class FriendsBloc extends Bloc<FriendsEvent, Map>{
+class FriendsBloc extends Bloc<FriendsEvent, FriendsState>{
 
   @override
-  Map get initialState => {};
+  FriendsState get initialState => FriendsLoading();
 
   @override
-  Stream<Map> mapEventToState(Map currentState, FriendsEvent event) async*{
+  Stream<FriendsState> mapEventToState(FriendsState currentState, FriendsEvent event) async*{
     if(event is AddFriends){
       yield* _mapAddFriendsToState(currentState,event);
     }
   }
 
-  Stream<Map>_mapAddFriendsToState(currentState ,event) async*{
-    // TODO pass back the new event without updating currentState so a transition gets triggered
+  Stream<FriendsState>_mapAddFriendsToState(currentState ,event) async*{
+    if(currentState is FriendsLoading){
+      currentState = {};
+    } else if(currentState is FriendsLoaded){
+      currentState = currentState.friends;
+    }
     final updatedFriends = Map.of(currentState);
     updatedFriends.addAll({event.key: event.value});
-    yield updatedFriends;
+    yield FriendsLoaded(updatedFriends);
   }
 }
-
-
-
--- lib/screens/friendsScreen.dart
+++ lib/screens/friendsScreen.dart
@@ -2,42 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:rallyapp/blocs/friends/friends.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class FriendsScreen extends StatelessWidget{
-
+class FriendsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _friendsBloc = BlocProvider.of<FriendsBloc>(context);
 
     return Scaffold(
-      body:BlocBuilder(
-        bloc: _friendsBloc,
-        builder: (BuildContext context, friends){
-          print('what is friends: in the friends screen $friends');
-          return Scaffold(
-            body: Container(
-              child: Text('$friends'),
-            ),
-          );
-        },
-      ) ,
+      body: BlocBuilder(
+          bloc: _friendsBloc,
+          builder: (BuildContext context, state) {
+            if (state is FriendsLoading) {
+              print('FriendsLoading...');
+              return new Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is FriendsLoaded) {
+              print('FriendsLoaded: ${state.friends.entries}');
+              return new Container(
+                  child: ListView(
+                      children: state.friends.entries
+                          .map<Widget>((item) => ListTile(
+                                leading: Container(
+                                    width: 40.0,
+                                    height: 40.0,
+                                    decoration: new BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: new DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: new NetworkImage(
+                                                item.value['userPhoto'])))),
+                                title: Text(item.value['userName']),
+                                trailing: Text(item.value['rallyID']),
+                              ))
+                          .toList()));
+            }
+          }),
     );
   }
 }
-
-
-//builder: (context, child, model) => ListView(
-//children: model.friendList.entries.map((item) => ListTile(
-//leading: Container(
-//width: 40.0,
-//height: 40.0,
-//decoration: new BoxDecoration(
-//shape: BoxShape.circle,
-//image: new DecorationImage(
-//fit: BoxFit.fill,
-//image: new NetworkImage(
-//item.value['userPhoto'])
-//)
-//)),
-//title: Text(item.value['userName']),
-//trailing: Text(item.value['rallyID']),
-//)).toList()
