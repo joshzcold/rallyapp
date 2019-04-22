bcb7622320f9a899cf4e7aeac9ea6def54e4f5f5
Joshua Cold <joshua.cold@microfocus.com>
2019-04-22 14:40:56 -0600

    seperated out the upload photo button into a differn't file

--- lib/widgets/uploadPhotoButton.dart
+++ lib/widgets/uploadPhotoButton.dart
@@ -6,39 +6,37 @@ uploadPhotoButton(theme, auth){
     mainAxisSize: MainAxisSize.max,
     children: <Widget>[
       Container(height: 20,),
-      Container(
+      InkWell(
+        onTap: (){
+
+        },
         child: Stack(
-          children: <Widget>[
-            InkWell(
-              onTap: (){
-
-              },
-              child: CircleAvatar(
-                backgroundColor: theme.theme['card'],
-                radius: 50,
-                backgroundImage: NetworkImage(auth.value['userPhoto']),
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(5.0),
+                child: CircleAvatar(
+                  backgroundColor: theme.theme['card'],
+                  radius: 50,
+                  backgroundImage: NetworkImage(auth.value['userPhoto']),
+                ),
               ),
-            ),
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: InkWell(
-                  onTap: () {
-
-                  },
+              
+              Positioned(
+                  bottom: 0,
+                  right: 0,
                   child: Row(
                     children: <Widget>[
                       Container(
                         decoration: BoxDecoration(color: theme.theme['colorPrimary'], borderRadius: BorderRadius.all(Radius.circular(100))),
-                        padding: EdgeInsets.all(10.0),
+                        padding: EdgeInsets.all(7.0),
                         child: Icon(Icons.camera, color: Colors.white,),
                       ),
                     ],
                   )
-              ),
-            )
-          ],
-        ),
+
+              )
+            ],
+          ),
       )
     ],
   );
