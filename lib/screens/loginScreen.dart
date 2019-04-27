c1f8a79d2268dc727837c6c6314efac5c2cc7cec
Joshua Cold <joshzcold@gmail.com>
2019-04-27 15:08:02 -0600

    Formatted the login page a bit better

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -184,61 +184,74 @@ class SignInPage extends StatelessWidget{
     var maxWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: theme.theme['background'],
-      body: ListView(
-        children: <Widget>[
-        Column(
-          mainAxisSize: MainAxisSize.max,
+      body: SingleChildScrollView(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Container(height: 100,),
+            Container(height: MediaQuery.of(context).size.height/4,),
             Form(
               key: _formKey,
-                  child: Column(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: maxWidth * .80,
+                    child: TextFormField(
+                      style: TextStyle(color: theme.theme['text']),
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                          ),
+                          hintText: 'Email',
+                          hintStyle: TextStyle(color: theme.theme['text'])
+                      ),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  Container(
+                    width: maxWidth * .80,
+                    child: TextFormField(
+                      obscureText: true,
+                      style: TextStyle(color: theme.theme['text']),
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                          ),
+                          hintText: 'Password',
+                          hintStyle: TextStyle(color: theme.theme['text'])
+                      ),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                      },
+                    ),
+                  ),
+                  Container(height: 10,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Container(
-                        width: maxWidth * .80,
-                        child: TextFormField(
-                          style: TextStyle(color: theme.theme['text']),
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                            ),
-                            hintText: 'Email',
-                            hintStyle: TextStyle(color: theme.theme['text'])
-                          ),
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return 'Please enter some text';
-                            }
+                      FlatButton(
+                          onPressed: ()  {
+                            Navigator.pushReplacementNamed(context, '/register');
                           },
-                        ),
-                      ),
-                      Container(
-                        height: 10,
-                      ),
-                      Container(
-                        width: maxWidth * .80,
-                        child: TextFormField(
-                          obscureText: true,
-                          style: TextStyle(color: theme.theme['text']),
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                          child: Container(
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                              color: theme.theme['colorSecondary'],
                             ),
-                            hintText: 'Password',
-                              hintStyle: TextStyle(color: theme.theme['text'])
-                          ),
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                          },
-                        ),
+                            child: Text('SIGN UP', style: TextStyle(color: Colors.white),),
+                          )
                       ),
-                      Container(height: 10,),
                       FlatButton(
                           onPressed: () async {
                             if (_formKey.currentState.validate()) {
@@ -248,50 +261,33 @@ class SignInPage extends StatelessWidget{
                           child: Container(
                             padding: EdgeInsets.all(10.0),
                             decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
                               color: theme.theme['colorPrimary'],
                             ),
                             child: Text('SIGN IN', style: TextStyle(color: Colors.white),),
                           )
-                        ),
+                      ),
                     ],
                   ),
-                ),
-
-            Container(
-              height: 30,
-            ),
-            FlatButton(
-                onPressed: ()  {
-                  Navigator.pushReplacementNamed(context, '/register');
-                },
-                  child: Container(
-                    padding: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                      color: theme.theme['colorSecondary'],
-                    ),
-                    child: Text('SIGN UP', style: TextStyle(color: Colors.white),),
-                  )
+                ],
               ),
-
+            ),
             Container(height: 10,),
             FlatButton(
                 onPressed: (){
                   signInWithGoogle(context);
                 },
-                  child: Container(
-                    padding: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                      color: theme.theme['colorPrimary'],
-                    ),
-                    child: Text('SIGN IN WITH GOOGLE', style: TextStyle(color: Colors.white),),
-                  )
-              ),
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    color: theme.theme['colorPrimary'],
+                  ),
+                  child: Text('SIGN IN WITH GOOGLE', style: TextStyle(color: Colors.white),),
+                )
+            ),
           ],
         ),
-        ],
       )
     );
   }
